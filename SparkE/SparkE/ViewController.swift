ae993c83d60b9b7a39181e5a00bacdc309e27dd2
Cathy Han <cathyhan@berkeley.edu>
2020-12-26 16:04:40 -0800

    Add signup page and tinker textfields

--- SparkE/SparkE/ViewController.swift
+++ SparkE/SparkE/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+        
+    let allowedEmailChars = CharacterSet(charactersIn:"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxyz@")
+    let allowedPassChars = CharacterSet(charactersIn:"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxyz!@#$%^&*()-_+=")
+
+    func emailField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let components = string.components(separatedBy: allowedEmailChars)
+        let filtered = components.joined(separator: "")
+        
+        if string == filtered {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func passwordField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let components = string.components(separatedBy: allowedPassChars)
+        let filtered = components.joined(separator: "")
+        
+        if string == filtered {
+            return true
+        } else {
+            return false
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
